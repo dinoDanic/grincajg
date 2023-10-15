32fb87803b505d40237c736351cec33fe4283585
Dino Danic <dino.danic@gmail.com>
2023-10-15 18:33:47 +0200

    refresh tokens

--- apps/grincajg_api/lib/grincajg_api_web/auth/error_response.ex
+++ apps/grincajg_api/lib/grincajg_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule GrincajgApiWeb.Auth.ErrorResponse.Forbidden do
   defexception message: "You do not have access", plug_status: 403
 end
+
+defmodule GrincajgApiWeb.Auth.ErrorResponse.NotFound do
+  defexception message: "Not Found", plug_status: 404
+end
