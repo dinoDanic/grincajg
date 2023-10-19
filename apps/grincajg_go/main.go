4f187524afe67c6d762178f0e42789c65c9863f1
Dino Danic <dino.danic@gmail.com>
2023-10-19 21:04:19 +0200

    go rest grincajg

---
+++ apps/grincajg_go/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"grincajg_api/controller"
+	"grincajg_api/databse"
+	"grincajg_api/model"
+	"log"
+)
+
+func main() {
+	loadEnv()
+	loadDatabase()
+	serveApplication()
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func loadDatabase() {
+	database.Connect()
+	database.Database.AutoMigrate(&model.User{})
+	database.Database.AutoMigrate(&model.Entry{})
+}
+
+func serveApplication() {
+	router := gin.Default()
+
+	publicRoutes := router.Group("/auth")
+	publicRoutes.POST("/register", controller.Register)
+	publicRoutes.POST("/login", controller.Login)
+
+	router.Run(":8000")
+	fmt.Println("Server running on port 8000")
+}
