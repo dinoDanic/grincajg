2ba0473fefccb68a4cdc19c0af15a6fe04c75a19
Dino Danic <dino.danic@gmail.com>
2023-10-21 19:56:54 +0200

    testing todos graphql

--- apps/grincajg_go_graphql/graph/resolver.go
+++ apps/grincajg_go_graphql/graph/resolver.go
@@ -1,7 +1,13 @@
 package graph
 
+import "grincajg/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+//go:generate go run github.com/99designs/gqlgen generate
+
+type Resolver struct {
+	todos []*model.Todo
+}
