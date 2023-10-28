25a1866fd19d6d016d04f0f7069bdf091eb61dbb
Dino Danic <dino.danic@gmail.com>
2023-10-28 18:20:26 +0200

    graphql ;

---
+++ apps/grincajg_go_graphql/database/migration/000001_create-user.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    verified BOOLEAN NOT NULL DEFAULT false,
+    super_admin BOOLEAN NOT NULL DEFAULT false,
+    organization_id INT,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
